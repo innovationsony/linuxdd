c7a3529c78515ad3e9c2d6b5a27745d8cf2b2467
innovationsony <innnovationsony@gmail.com>
2015-09-03 22:32:19 +0530

    folder

---
+++ folder/soc1_send.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include <sys/types.h>          /* See NOTES */
+#include <sys/socket.h>
+
+
+//int socket(int domain, int type, int protocol);
+
